d4f1796f404d5586ab6fdacd7b8e56be2f2c0729
Juan José Fernández <ponisaurio079@hotmail.com>
2025-08-14 18:59:15 -0300

    working
    working

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,29 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+
+document.getElementById("regBtn").addEventListener("click", function(e) {
+      e.preventDefault();
+    
+    let nombre = document.getElementById("nombre").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let pass1 = document.getElementById("password1").value;
+    let pass2 = document.getElementById("password2").value;
+    let terminos = document.getElementById("terminos").checked;
+
+    
+    if (
+        nombre === "" ||
+        email === "" ||
+        pass1 === "" ||
+        pass2 === "" ||
+        pass1.length < 6 ||
+        pass1 !== pass2 ||
+        !terminos
+    ) {
+        showAlertError();
+    } else {
+        showAlertSuccess();
+    }
+});
