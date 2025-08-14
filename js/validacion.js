8da463e54c68407a73d9cf09c9cc2cf325cf66bf
maxiigoomez <maxiigoomez1995@gmail.com>
2025-08-14 19:08:18 -0300

    actualizado

--- js/validacion.js
+++ js/validacion.js
@@ -1,3 +1,24 @@
+//verificar si los campos estan vacios
+function validarForm() {
+    let nombre = document.getElementById("nombre").value;
+    let apellido = document.getElementById("apellido").value;
+    let email = document.getElementById("email").value;
+    let password1 = document.getElementById("password1").value;
+    let password2 = document.getElementById("password2").value;
+    let terminos = document.getElementById("terminos").checked;
+    let error = false;
+    if (nombre === "" || apellido === "" || email === "" || password1 === "" || password2 === "" || password1!==password2 || !terminos) {
+        error = true;
+    }
+    if (error) {
+        showAlertError();
+        return false;
+    } else {
+        showAlertSuccess();
+        return true;
+    }
+}
+
 function showAlertSuccess() {
     document.getElementById("alert-success").classList.add("show");
 }
@@ -5,3 +26,10 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+
+// Agregar el evento al botón de registro
+let miBoton = document.getElementById("regBtn");
+miBoton.addEventListener('click', function() {
+  validarForm();
+});
